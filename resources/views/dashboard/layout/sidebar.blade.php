b9c59a3a46eb2de5d41767c3f26c4d4429c8ac55
Mayank Sharma <makedy07feb@gmail.com>
2024-08-08 17:59:52 +0530

    user-profile-page

--- resources/views/dashboard/layout/sidebar.blade.php
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -2,13 +2,21 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-             style="opacity: .8">
+        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="{{route('userprofile')}}" class="d-block">{{auth()->user()->name}}</a>
+            </div>
+        </div>
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
@@ -16,53 +24,50 @@
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
                 <li class="nav-item has-treeview menu-open">
-                    <a href="{{route('home')}}" class="nav-link ">
+                    <a href="{{route('home')}}" class="nav-link active">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Dashboard
                         </p>
                     </a>
-
                 </li>
                 <li class="nav-item">
                     <a href="{{route('attendance.index')}}" class="nav-link">
-                        <i class="nav-icon fas fa-th"></i>
+                        <i class="nav-icon fas fa-calendar-alt"></i>
                         <p>
                             Records
                         </p>
                     </a>
                 </li>
                 @role('super_admin|admin')
-                    <li class="nav-item">
-                        <a href="{{route('employee.index')}}" class="nav-link">
-                            <i class="nav-icon fas fa-th"></i>
-                            <p>
-                                Employee
-                            </p>
-                        </a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a href="{{route('office.index')}}" class="nav-link">
-                            <i class="nav-icon fas fa-th"></i>
-                            <p>
-                                Office
-                            </p>
-                        </a>
-                    </li>
+                <li class="nav-item">
+                    <a href="{{route('employee.index')}}" class="nav-link">
+                        <i class="nav-icon fas fa-user-tie"></i>
+                        <p>
+                            Employee
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('office.index')}}" class="nav-link">
+                        <i class="nav-icon fas fa-building"></i>
+                        <p>
+                            Office
+                        </p>
+                    </a>
+                </li>
                 @endrole
-
                 <li class="nav-item">
                     <form action="{{route('logout')}}" method="post">
                         @csrf
-                        <button type="submit" class="nav-link"><i class="nav-icon fas fa-th"></i>
+                        <button type="submit" class="nav-link">
+                            <i class="nav-icon fas fa-sign-out-alt"></i>
                             <p>
-                                logout
+                                Logout
                             </p>
                         </button>
                     </form>
                 </li>
-
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
