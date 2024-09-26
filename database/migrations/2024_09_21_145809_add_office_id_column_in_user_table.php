08205e0d85ceb1698a35fb8422a1215e5f5801c9
tabrej03dz <tabrejalam770496@gmail.com>
2024-09-26 10:33:47 +0530

    adding office_id column in users table

--- database/migrations/2024_09_21_145809_add_office_id_column_in_user_table.php
+++ database/migrations/2024_09_21_145809_add_office_id_column_in_user_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-//            $table->unsignedBigInteger('office_id')->nullable();
-//            $table->foreign('office_id')->references('id')->on('offices')->onDelete('SET NULL');
+            $table->unsignedBigInteger('office_id')->nullable();
+            $table->foreign('office_id')->references('id')->on('offices')->onDelete('SET NULL');
         });
     }
 
