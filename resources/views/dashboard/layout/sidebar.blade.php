7a94ff1e7e0134343c3658f413172e2ce1b0e1e3
chandan <chandandigital6@gmail.com>
2024-10-04 13:21:39 +0530

    add sidebar active menu

--- resources/views/dashboard/layout/sidebar.blade.php
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -1,104 +1,74 @@
-<!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-{{--    <!-- Brand Logo -->--}}
-{{--    <a href="" class="brand-link">--}}
-{{--        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">--}}
-{{--        <span class="brand-text font-weight-light">AdminLTE 3</span>--}}
-{{--    </a>--}}
-
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-{{--            <div class="image">--}}
-{{--                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">--}}
-{{--            </div>--}}
             <div class="info">
-                <a href="{{route('userprofile', ['user' => auth()->user()->id])}}" class="d-block">{{auth()->user()->name}}</a>
+                <a href="{{ route('userprofile', ['user' => auth()->user()->id]) }}" class="d-block">{{ auth()->user()->name }}</a>
             </div>
         </div>
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <!-- Add icons to the links using the .nav-icon class
-                     with font-awesome or any other icon font library -->
-                <li class="nav-item has-treeview menu-open">
-                    <a href="{{route('home')}}" class="nav-link active">
+                <li class="nav-item has-treeview {{ request()->routeIs('home') ? 'menu-open' : '' }}">
+                    <a href="{{ route('home') }}" class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>
-                            Dashboard
-                        </p>
+                        <p>Dashboard</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{route('attendance.index')}}" class="nav-link">
+                    <a href="{{ route('attendance.index') }}" class="nav-link {{ request()->routeIs('attendance.index') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-calendar-alt"></i>
-                        <p>
-                            Records
-                        </p>
+                        <p>Records</p>
                     </a>
                 </li>
-{{--                @role('admin|super_admin')--}}
+                @role('admin|super_admin')
                 <li class="nav-item">
-                    <a href="{{route('attendance.day-wise')}}" class="nav-link">
+                    <a href="{{ route('attendance.day-wise') }}" class="nav-link {{ request()->routeIs('attendance.day-wise') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-calendar-alt"></i>
-                        <p>
-                            Attendance
-                        </p>
+                        <p>Attendance</p>
                     </a>
                 </li>
-{{--                @endrole--}}
+                @endrole
                 @role('super_admin|admin')
                 <li class="nav-item">
-                    <a href="{{route('employee.index')}}" class="nav-link">
+                    <a href="{{ route('employee.index') }}" class="nav-link {{ request()->routeIs('employee.index') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-user-tie"></i>
-                        <p>
-                            Employee
-                        </p>
+                        <p>Employee</p>
                     </a>
                 </li>
                 @endrole
                 @role('super_admin|admin|team_leader')
                 <li class="nav-item">
-                    <a href="{{route('leave.index')}}" class="nav-link">
+                    <a href="{{ route('leave.index') }}" class="nav-link {{ request()->routeIs('leave.index') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-user-tie"></i>
-                        <p>
-                            Leave
-                        </p>
+                        <p>Leave</p>
                     </a>
                 </li>
                 @endrole
                 @role('super_admin')
                 <li class="nav-item">
-                    <a href="{{route('office.index')}}" class="nav-link">
+                    <a href="{{ route('office.index') }}" class="nav-link {{ request()->routeIs('office.index') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-building"></i>
-                        <p>
-                            Office
-                        </p>
+                        <p>Office</p>
                     </a>
                 </li>
                 @endrole
-
                 @role('super_admin|admin')
                 <li class="nav-item">
-                    <a href="{{route('off.index')}}" class="nav-link">
+                    <a href="{{ route('off.index') }}" class="nav-link {{ request()->routeIs('off.index') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-building"></i>
-                        <p>
-                            Manage Offs
-                        </p>
+                        <p>Manage Offs</p>
                     </a>
                 </li>
                 @endrole
-
                 <li class="nav-item">
-                    <form action="{{route('logout')}}" method="post">
+                    <form action="{{ route('logout') }}" method="post">
                         @csrf
                         <button type="submit" class="nav-link">
                             <i class="nav-icon fas fa-sign-out-alt"></i>
-                            <p>
-                                Logout
-                            </p>
+                            <p>Logout</p>
                         </button>
                     </form>
                 </li>
