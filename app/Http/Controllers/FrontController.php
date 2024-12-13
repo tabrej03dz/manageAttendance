509304e9fe87b5156cc58c8ae954fd7001409f6a
tabrej03dz <tabrejalam770496@gmail.com>
2024-12-13 11:10:53 +0530

    exra middileware remove

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    public function mainpage()
+    {
+        return view('mainpage.index');
+    }
+
+    public function blogs()
+    {
+        return view('mainpage.blog');
+    }
+
+    public function blogDetailsPage()
+    {
+        return view('mainpage.blogDetailsPage');
+    }
+
+    public function reqDemo()
+    {
+        return view('mainpage.reqaDemo');
+    }
+
+}
