2a44887f352dc7bacae9dcedc33ead83166c0c97
tabrej03dz <tabrejalam770496@gmail.com>
2024-12-28 10:43:24 +0530

    login with token api

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\PersonalAccessToken;
+use Illuminate\Support\Facades\Auth;
+
+
 
 
 
@@ -19,9 +23,6 @@ class AuthController extends Controller
             'email' => 'required|email',
             'password' => 'required|string|min:6',
         ]);
-
-
-
         // Attempt to authenticate the user
         $user = User::where('email', $request->email)->first();
 //        return response($user);
@@ -44,6 +45,33 @@ class AuthController extends Controller
 
         return response()->json([
             'message' => 'Logged out successfully',
+        ], 200);
+    }
+
+
+    public function tokenLogin(Request $request)
+    {
+        // Validate input
+        $request->validate([
+            'token' => 'required|string',
+        ]);
+
+
+        // Extract the token from the request
+        $token = $request->token;
+
+
+        // Attempt to authenticate the user using the token
+        $user = PersonalAccessToken::findToken($token)?->tokenable;
+
+
+        if (!$user || !Auth::loginUsingId($user->id)) {
+            return response()->json(['message' => 'Invalid or expired token'], 401);
+        }
+
+        return response()->json([
+            'user' => $user,
+            'message' => 'Authenticated successfully using token',
         ], 200);
     }
 }
