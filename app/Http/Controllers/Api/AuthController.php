f49781a7b0e86b0153a7c6e6eceb152a1aa3b742
tabrej03dz <tabrejalam770496@gmail.com>
2025-01-12 13:57:12 +0530

    login api changes

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -18,6 +18,7 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
+        return response($request->all());
         // Validate input
         $request->validate([
             'email' => 'required|email',
