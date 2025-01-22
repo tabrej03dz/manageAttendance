443e4bdfb4f31370a756e333d9699566447e81c9
mayank07feb <makedy07feb@gmail.com>
2025-01-22 13:03:21 +0530

    login

--- resources/views/mainpage/components/header.blade.php
+++ resources/views/mainpage/components/header.blade.php
@@ -8,28 +8,12 @@
          <div class="logo flex items-center justify-start cursor-pointer">
              <a href="{{ route('mainpage') }}">
                  <div>
-                     {{-- <svg class="drop-shadow-md" preserveAspectRatio="xMidYMid meet" data-bbox="0 0 142 85"
-                         viewBox="0 0 142 85" height="85" width="70" xmlns="http://www.w3.org/2000/svg"
-                         data-type="color" role="img" aria-label="AttendNow">
-                         <g>
-                             <path fill="#BB2323"
-                                 d="M86 42.5C86 65.972 66.748 85 43 85S0 65.972 0 42.5 19.252 0 43 0s43 19.028 43 42.5z"
-                                 data-color="1"></path>
-                             <path fill="#DC2626"
-                                 d="M142 42.298c0 23.36-18.937 42.298-42.298 42.298-23.36 0-42.298-18.937-42.298-42.298C57.404 18.938 76.341 0 99.702 0 123.062 0 142 18.937 142 42.298z"
-                                 data-color="2"></path>
-                             <path fill="#ffffff"
-                                 d="M81.404 39.968 95.656 54.22a3.848 3.848 0 1 1-5.442 5.442L75.963 45.41a3.848 3.848 0 1 1 5.442-5.442z"
-                                 data-color="3"></path>
-                             <path fill="#ffffff"
-                                 d="M124.44 30.855 95.678 59.62a3.848 3.848 0 1 1-5.442-5.442L119 25.413a3.848 3.848 0 1 1 5.442 5.442z"
-                                 data-color="3"></path>
-                         </g>
-                     </svg> --}}
-                   
-                     <a href="{{ route('mainpage') }}"><img src="{{asset('asset/img/logo (2).png')}}" alt="" class="h-24 w-24">
+                    
+
+                     <a href="{{ route('mainpage') }}"><img src="{{ asset('asset/img/logo (2).png') }}" alt=""
+                             class="h-24 w-24">
                      </a>
-                    </div>
+                 </div>
                  {{-- <div class="text-paraTextxl ml-3">
                      <h2 class="text-bgHeader font-futuraBk text-xl md:text-2xl tracking-wide"><a
                              href="{{ route('mainpage') }}">AttendNow</a></h2>
@@ -38,7 +22,9 @@
          </div>
 
          {{-- button --}}
-         <div class="block md:hidden w-full flex justify-center"> <a class="Login bg-red-600 rounded-lg p-2 px-4 font-bold text-white text-lg" href="{{ route('login') }}">Login</a></div>
+         <div class="block md:hidden w-full flex justify-center"> <a
+                 class="Login bg-red-600 rounded-lg p-2 px-4 font-bold text-white text-lg"
+                 href="{{ route('login') }}">Login</a></div>
 
          <!-- main navbar -->
          <nav class="flex items-center justify-center">
@@ -56,33 +42,11 @@
                          </li>
                          {{-- <li><a href="{{ route('login') }}">Login</a></li> --}}
                      </ul>
-                    
-                     <div class="w-[152px] h-11 flex justify-center bg-gradient-to-t from-red-500 to-black text-white hover:text-white hover:bg-red-500 rounded-xl p-2 text-lg "> <a href="{{ route('login') }}">Login</a></div>
-                    
-                     {{-- <form
-                         class="group w-[152px] h-11 max-w-sm bg-bgSecondary outline-none focus:ring-1 focus:ring-bgSecondary font-futuraBk rounded-full inline-flex cursor-pointer">
-                         <select id="dropdownDividerButton" data-dropdown-toggle="dropdownDivider"
-                             class="bg-bgSecondary text-white text-base px-1 ml-5 py-2 outline-none border-none cursor-pointer *:border-b *:border-b-bgHeader">
 
-                             <option value="English" selected>English</option>
-                             <!-- <option value="lang" class="opacity-50">Choose a Language</option> -->
-                             <option value="العربية">العربية</option>
-                             <option value="বাংলা">বাংলা</option>
-                             <option value="Español">Español</option>
-                             <option value="Français">Français</option>
-                             <option value="ગુજરાતી">ગુજરાતી</option>
-                             <option value="हिन्दी">हिन्दी</option>
-                             <option value="ಕನ್ನಡ">ಕನ್ನಡ</option>
-                             <option value="മലയാളം">മലയാളം</option>
-                             <option value="मराठी">मराठी</option>
-                             <option value="ਪੰਜਾਬੀ">ਪੰਜਾਬੀ</option>
-                             <option value="Português">Português</option>
-                             <option value="Русский">Русский</option>
-                             <option value="தமிழ்">தமிழ்</option>
-                             <option value="తెలుగు">తెలుగు</option>
-                             <option value="اردو">اردو</option>
-                         </select>
-                     </form> --}}
+                     <div
+                         class="w-[152px] h-11 flex justify-center bg-gradient-to-t from-red-500 to-black text-white hover:text-white hover:bg-red-500 rounded-xl p-2 text-lg ">
+                         <a href="{{ route('login') }}">Login</a></div>
+
                  </div>
              </div>
          </nav>
@@ -105,36 +69,12 @@
                      </ul>
 
                      <div>
-                         {{-- <form
-                             class="group w-[152px] h-11 max-w-sm bg-bgSecondary outline-none focus:ring-1 focus:ring-bgSecondary font-futuraBk rounded-full inline-flex cursor-pointer">
-                             <select id="dropdownDividerButton" data-dropdown-toggle="dropdownDivider"
-                                 class="bg-bgSecondary text-white text-base px-1 ml-5 py-2 outline-none border-none cursor-pointer *:border-b *:border-b-bgHeader">
-
-                                 <option value="English" selected>English</option>
-                                 <!-- <option value="lang" class="opacity-50">Choose a Language</option> -->
-                                 <option value="العربية">العربية</option>
-                                 <option value="বাংলা">বাংলা</option>
-                                 <option value="Español">Español</option>
-                                 <option value="Français">Français</option>
-                                 <option value="ગુજરાતી">ગુજરાતી</option>
-                                 <option value="हिन्दी">हिन्दी</option>
-                                 <option value="ಕನ್ನಡ">ಕನ್ನಡ</option>
-                                 <option value="മലയാളം">മലയാളം</option>
-                                 <option value="मराठी">मराठी</option>
-                                 <option value="ਪੰਜਾਬੀ">ਪੰਜਾਬੀ</option>
-                                 <option value="Português">Português</option>
-                                 <option value="Русский">Русский</option>
-                                 <option value="தமிழ்">தமிழ்</option>
-                                 <option value="తెలుగు">తెలుగు</option>
-                                 <option value="اردو">اردو</option>
-                             </select>
-                         </form> --}}
 
                          <div class="infoText text-bgSecondary mt-16">
                              <h6 class="pb-4 text-lg text-textSecondary font-futuraBk font-medium">Call or Email Us
                              </h6>
                              <a class="text-2xl font-futuraMd font-semibold" href="tel:+917753800444">+91
-                                7753800444</a>
+                                 7753800444</a>
                              <a class="pt-2 block text-xl font-futuraMd font-semibold"
                                  href="#">info@realvictorygroups</a>
                          </div>
@@ -142,7 +82,7 @@
 
 
                  </div>
-                 
+
              </div>
          </div>
 
