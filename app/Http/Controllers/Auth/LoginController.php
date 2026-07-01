424ccebbbf74edee5da31683a3c2878ee29802f6
tabrej03dz <tabrejalam770496@gmail.com>
2026-07-01 11:18:49 +0530

    slkdjf

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -178,7 +178,7 @@ class LoginController extends Controller
 
     $request->session()->regenerate();
 
-    return redirect()->intended('/dashboard')
+    return redirect()->intended('/home')
         ->with('success', 'Login successful.');
 }
 
